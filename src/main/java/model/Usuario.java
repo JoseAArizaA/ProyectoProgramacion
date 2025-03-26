7a7fe9d85fbbf9a127eef16e8525eb7f78ae2412
Javier~ <jguehur988@g.educaand.es>
2025-03-26 20:22:44 +0100

    Actuliazacion de las clases pricipales

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,4 +1,48 @@
-package model;
+package Model;
 
-public class Usuario {
+public abstract class Usuario {
+    protected String nombre;
+    protected String usuario;
+    protected String contrasena;
+    protected String email;
+    public Usuario(String nombre, String usuario, String contrasena, String email) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public abstract String getRol();
 }
