c72211b1aaf5ed36ae23e8621498189ded0de9d9
JoseA <joseantonioariza004@gmail.com>
2025-03-27 09:34:36 +0100

    Corregir nombre de paquetes

--- src/main/java/model/Iniciativa.java
+++ src/main/java/model/Iniciativa.java
@@ -1,4 +1,8 @@
 package model;
 
+import java.util.List;
+
 public class Iniciativa {
+    public Iniciativa(String nombre, String descripcion, CreadorIniciativa creador, List<Actividad> actividades) {
+    }
 }
