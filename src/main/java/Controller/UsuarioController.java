c680fbe506d4ae38836e1f90fd0d400fe4eb9ea3
JoseA <joseantonioariza004@gmail.com>
2025-03-28 08:39:38 +0100

    Corregir nombre de paquetes

--- src/main/java/Controller/UsuarioController.java
+++ src/main/java/Controller/UsuarioController.java
@@ -1,19 +1,37 @@
 package controller;
 
 import model.Usuario;
-import view.View;
+import utils.XMLManager;
+import view.ViewRegistro;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UsuarioController {
-    private static List<Usuario> usuarios = new ArrayList<>();
+    private static Set<Usuario> usuarios = new HashSet<>();
 
     public static void registrarUsuario() {
-        Usuario usuario = View.pideDatosUsuario();
+        Usuario usuario = ViewRegistro.pideDatosUsuario();
 
-
+        if (usuario != null) {
+            usuarios.add(usuario);
+            System.out.println("Usuario registrado con éxito");
+        } else {
+            System.out.println("Error al registrar usuario");
+        }
 
     }
+
+    public static void guardarUsuarios(String UsuariosXML) {
+        if (XMLManager.writeXML(usuarios, UsuariosXML)) {
+            System.out.println("Los usuarios se han guardado");
+        } else {
+            System.out.println("Error al guardar los usuarios");
+        }
+
+    }
+
 
 }
