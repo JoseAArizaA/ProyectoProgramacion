b3e02f202e4ef7f1053758e7a8180ab9a420c380
Javier~ <jguehur988@g.educaand.es>
2025-04-01 20:28:11 +0200

    1 de Abril 2.0

--- src/main/java/controller/IniciativaController.java
+++ src/main/java/controller/IniciativaController.java
@@ -1,11 +1,5 @@
 package controller;
 
-import model.Iniciativa;
-import model.Actividad;
-import model.Voluntario;
-import model.EstadoActividad;
-
-import java.time.LocalDate;
 
 public class IniciativaController {
 
--- src/main/java/controller/VoluntarioController.java
+++ src/main/java/controller/VoluntarioController.java
@@ -1,13 +1,8 @@
 package controller;
 
-import model.EstadoActividad;
-import model.Voluntario;
-import model.Actividad;
 
-import java.util.List;
 
 public class VoluntarioController {
-    private Voluntario voluntario;
 
 }
 
